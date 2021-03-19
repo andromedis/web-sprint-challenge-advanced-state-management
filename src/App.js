b896052da701929721009e9bc93c946f47597623
Rebecca Peckman <rebeccapeckman@gmail.com>
2021-03-19 16:13:04 +0000

    connected fetchSmurfs to App, will be called when App mounts

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
+// Libraries
 import React, { Component } from "react";
+import { connect } from 'react-redux';
 
+// Actions
+import { fetchSmurfs } from './actions';
+
+// Components
 import AddForm from './components/AddForm';
 import SmurfList from './components/SmurfList';
 import Header from './components/Header';
 
+// Styles
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./App.css";
 
 class App extends Component {
+
+  componentDidMount() {
+    fetchSmurfs();
+  }
+
   render() {
     return (
       <div className="App">
@@ -22,7 +34,15 @@ class App extends Component {
   }
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  return {
+    smurfs: state.smurfs,
+    isLoading: state.isLoading,
+    error: state.error,
+  }
+}
+
+export default connect(mapStateToProps, { fetchSmurfs })(App);
 
 //Task List:
 //1. Connect the fetchSmurfs actions to the App component.
